2bc2dfe7dbaf755f6f09a0084f9110a58637c60f
aslikarahan <asikarahan96@gmail.com>
2021-02-20 19:59:12 +0100

    generic form filler and checkbox checker

--- src/__main__.py
+++ src/__main__.py
@@ -17,58 +17,76 @@ class BrowserLogin:
         self.email_address = "cookiehunterproject@gmail.com"
         self.pwd = "passwordRandom123!"
         self.name = "Cookie Hunter"
-
+        self.username = "CookieHunter007"
+        self.email_synonyms = ['user_email', 'email', 'e_mail', 'useremail', 'userEmail', 'mail', 'uemail',
+                         'User_email', 'Email', 'E_mail', 'Useremail', 'UserEmail', 'Mail', 'Uemail',
+                         'User_email_address', 'Email_address', 'email_address', 'emailadress', 'UserEmailAddress',
+                         'MailAdress'
+                         ]
+        self.password_synonyms = ['user_password', 'password', 'pword', 'userpassword', 'userpwd', 'pwd', 'PWD',
+                         'u_password', 'passw', 'p_word', 'UserPassword', 'UserPwd', 'Pwd', 'pass',
+                         ]
+        self.name_synonyms = ['name_first', 'name', 'first_name', 'firstname', 'First_Name', 'f_name', 'firstName',
+                               'User_efirstname', 'First_name', 'first_Name', 'NAME', 'F_NAME', 'FName',
+                               'fname_', '_firstname', 'fullname', 'full_name',
+                               ]
+        self.username_synonyms = ['username', 'uname', 'user_id', 'user_name', 'uName', 'u_Name', 'UserName',
+                               'user_name_new', 'new_username', 'user_username', 'user_username', 'user[username]'
+                               ]
 
     def register(self):
         self.browser.get((self.register_url))
-        email = self.email_el()
-        pwd = self.pwd_el()
-        name = self.name_el()
+        # login_form = self.browser.find_element_by_xpath("//form[1]")
+        checks = self.browser.find_elements(By.XPATH, "//input[@type='checkbox']")
+        for check in checks:
+            check.click()
 
-        email.send_keys(self.email_address)
-        pwd.send_keys(self.pwd)
-        name.send_keys(self.name)
+        email = self.generic_element_finder("//input[@type='email']", self.email_synonyms)
+        if email is not None:
+            email.send_keys(self.email_address)
 
+        pwd = self.generic_element_finder("//input[@type='password']", self.password_synonyms)
+        if pwd is not None:
+            pwd.send_keys(self.pwd)
+
+        name = self.generic_element_finder("//input[@type='name']", self.name_synonyms)
+        if name is not None:
+            name.send_keys(self.name)
+
+        username = self.generic_element_finder("//input[@type='username']", self.username_synonyms)
+        if username is not None:
+            username.send_keys(self.username)
+
+        # button = self.browser.find_element(By.XPATH, "//button[@type='submit']")
+        # button.click()
+        print("form filling complete")
         # name.submit()
         # button = browser.findElement(By.xpath("//button[text()='Sign up']")).click();
         # button.click()
 
-    def email_el(self):
-        '''
-        Searches page set on browser for email html element
-        :return: email input field
-        '''
-        email = None
+    def generic_element_finder(self, x_path_text, text_list):
+        element = None
         try:
-            email = self.browser.find_element_by_id('email')
+            element = self.browser.find_element(By.XPATH, x_path_text)
         except Exception as e:
-            print(e)
-        return email
+            pass
+        for text in text_list:
+            try:
+                element = self.browser.find_element(By.NAME, text)
+            except Exception as e:
+                pass
+                # print(e)
+            try:
+                element = self.browser.find_element(By.TAG_NAME, text)
+            except Exception as e:
+                pass
+                # print(e)
+            try:
+                element = self.browser.find_element(By.ID, text)
+            except Exception as e:
+                pass
 
-    def pwd_el(self):
-        '''
-        Searches page set on browser for password html element
-        :return: password input field
-        '''
-        pwd = None
-        try:
-            pwd = self.browser.find_element_by_id('password')
-        except Exception as e:
-            print(e)
-        return pwd
-
-    def name_el(self):
-        '''
-        Searches page set on browser for name html element
-        :return: input field for name
-        '''
-        # TODO: split into first and last name
-        name = None
-        try:
-            name = self.browser.find_element_by_id('name')
-        except Exception as e:
-            print(e)
-        return name
+        return element
 
 
 if __name__ == "__main__":
@@ -79,7 +97,7 @@ if __name__ == "__main__":
     # browser = BrowserLogin(home_url=home_url, login_url=login_url, register_url=register_url, driver_path=PATH)
     # browser.register()
 
-    with open('data/sites.json') as sites_file:
+    with open('../data/sites.json') as sites_file:
         sites = json.load(sites_file)
         for site in sites:
             browser = BrowserLogin(home_url=site['home_url'], login_url=site['login_url'], register_url=site['register_url'], driver_path=PATH)
