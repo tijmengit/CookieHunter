881ba67c3387a7627e55461b99ed29ebe44f30f5
Stijn Kramer <stijn_kramer@hotmail.com>
2021-02-23 22:18:59 +0100

    PrivacyAuditor - refactor + multiprocessing

--- src/PrivacyAuditor.py
+++ src/PrivacyAuditor.py
@@ -7,11 +7,14 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup as BS
 import urllib.parse as urlparse
 from pprint import pprint
+import multiprocessing as MP
 
 
 class PrivacyAuditor:
     keywords = []
-    ignore = ['logout', 'login', 'register', 'stories', 'blog', 'forum', 'campaigns']
+    ignore = ['logout', 'login', 'register', 'sign-in', 'sign-out', 'stories', 'blog', 'forum', 'campaigns',
+              'book', 'books', 'genres', 'product', 'products', 'news', 'stories', 'story', 'choiceawards',
+              'list', 'reviews', 'review', 'quotes', 'quote', 'releases', 'films', 'movies', 'film', 'movie']
 
     def __init__(self, driver_path: str, base_url: str, cookies: List[Dict[str, str]], auth: bool,
                  browser_options: List[str]):
@@ -24,13 +27,24 @@ class PrivacyAuditor:
             self.browser_options.add_argument(option)
 
     def audit(self):
-        depth = 1
-        without_cookies = self.get_sitemap(False, depth)
-        with_cookies = self.get_sitemap(True, depth)
-        pprint(set(with_cookies) - set(without_cookies))
+        pages = self.get_interesting_pages()
 
-    def get_sitemap(self, use_cookies: bool, depth: int):
-        pprint(f'Fetching sitemap for {self.base_url} with{"out" if not use_cookies else ""} cookies...')
+    def get_interesting_pages(self):
+        depth = 1
+        manager = MP.Manager()
+        without_cookies = manager.list()
+        with_cookies = manager.list()
+        p1 = MP.Process(target=self.get_sitemap, args=(False, depth, without_cookies))
+        p2 = MP.Process(target=self.get_sitemap, args=(True, depth, with_cookies))
+        p1.start()
+        p2.start()
+        p1.join()
+        p2.join()
+        without_cookies = without_cookies[:]
+        with_cookies = with_cookies[:]
+        return set(with_cookies) - set(without_cookies)
+
+    def get_sitemap(self, use_cookies: bool, depth: int, return_value: List):
         browser = webdriver.Chrome(self.driver_path, options=self.browser_options)
         base = urlparse.urlparse(self.base_url).netloc
 
@@ -47,24 +61,24 @@ class PrivacyAuditor:
         # TODO fix to ignore pages like productlists (e.g. "https://something.com/products/<id:0-9999999999>")
 
         while queue:
-            url = queue.pop(0)
-            browser.get(url[0])
+            page = queue.pop(0)
+            browser.get(page[0])
             soup = BS(browser.page_source, 'html.parser')
             for link in soup.find_all('a'):
-                l = urlparse.urljoin(self.base_url, link.get('href'))
-                parsed = urlparse.urlparse(l)
-                path = parsed.path.split('/')
+                full_link = urlparse.urljoin(self.base_url, link.get('href'))
+                parsed_link = urlparse.urlparse(full_link)
+                path = parsed_link.path.split('/')
                 elem = path[1] if len(path) >= 2 else ''
-                if parsed.netloc == base \
+                if parsed_link.netloc == base \
                         and elem not in self.ignore \
-                        and url[1] < depth \
-                        and l not in found:
-                    found.append(l)
-                    queue.append((l, url[1] + 1))
+                        and page[1] < depth \
+                        and full_link not in found:
+                    found.append(full_link)
+                    queue.append((full_link, page[1] + 1))
                     queue = sorted(queue, key=lambda x: x[1])
 
         browser.quit()
-        return found
+        return_value.extend(found)
 
 
 if __name__ == '__main__':
@@ -72,7 +86,7 @@ if __name__ == '__main__':
         'LOCALAPPDATA') + '/ChromeDriver/chromedriver' if platform.system() == 'Windows' else '/usr/local/sbin/chromedriver'
     url = 'https://www.goodreads.com/'
     stolen_cookies = [
-        {'name': '_session_id2', 'value': 'dad38f1f6290312d962baae42e7e3aec', 'domain': 'www.goodreads.com'},
+        {'name': '_session_id2', 'value': 'ea8c83a1a1893ab157dbf0069ada1e52', 'domain': 'www.goodreads.com'},
         {'name': 'locale', 'value': 'en', 'domain': 'www.goodreads.com'},
         {'name': 'logged_out_browsing_page_count', 'value': '2', 'domain': 'www.goodreads.com'},
         {'name': 'cssid', 'value': '848-2350035-9701439', 'domain': 'www.goodreads.com'},
