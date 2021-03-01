df645d712ce5daee20ea89d5c3db4e912b1f96da
schorliee <da.charlie.nguyen@gmail.com>
2021-03-01 01:09:37 +0100

    Finished Function to find authentication Cookies

--- src/__main__.py
+++ src/__main__.py
@@ -9,15 +9,17 @@ if __name__ == "__main__":
     login_url = "https://www.goodreads.com/user/sign_in"
     register_url = "https://www.goodreads.com/user/sign_up"
     PATH = os.getenv(
-        "LOCALAPPDATA") + "/ChromeDriver/chromedriver" if platform.system() == "Windows" else "/usr/local/sbin/chromedriver"
+        "LOCALAPPDATA") + "/ChromeDriver/chromedriver" if platform.system() == "Windows" else "../chromedriver"
     browser = Browser(home_url=home_url, login_url=login_url, register_url=register_url, driver_path=PATH)
 
     try:
         # browser.register()
         browser.login()
         c_auditor = CookieAuditor(browser)
-        vulnerable = c_auditor.findVulnerableCookies()
-        print(vulnerable)
+        #vulnerable = c_auditor.findVulnerableCookies()
+        #print(vulnerable)
+        auth_cookies = c_auditor.findAuthCookies()
+        print(auth_cookies)
         browser.close()
     except:
         traceback.print_exc()
