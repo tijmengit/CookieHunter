24a2f92959246d0c04c7f2eadb285ed191cb6619
tijmen104 <tijmen104@gmail.com>
2021-03-01 16:54:59 +0100

    Dev general url identifier

--- src/__main__.py
+++ src/__main__.py
@@ -7,7 +7,7 @@ from src.CookieAuditor import *
 if __name__ == "__main__":
     home_url = ""
     login_url = ""
-    register_url = "https://hypothes.is/signup"
+    register_url = "https://www.amazon.nl/ap/register?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.nl%2F%3F_encoding%3DUTF8%26ref_%3Dnav_newcust&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=nlflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&"
     PATH = os.getenv(
         "LOCALAPPDATA") + "/ChromeDriver/chromedriver" if platform.system() == "Windows" else "/usr/local/sbin/chromedriver"
     browser = Browser(home_url=home_url, login_url=login_url, register_url=register_url, driver_path=PATH)
