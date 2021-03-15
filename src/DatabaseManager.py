4218b5ea4763dca0d2533d34e7f3b905d33814c0
vstam <just.van.stam@gmail.com>
2021-03-15 15:15:53 +0100

    fix database error

--- src/DatabaseManager.py
+++ src/DatabaseManager.py
@@ -5,9 +5,10 @@ from firebase_admin import firestore
 class DatabaseManager:
 
     def __init__(self):
-        cred = credentials.Certificate("./../data/database_cred.json")
-        firebase_admin.initialize_app(cred)
-        self.db = firestore.client()
+        if not firebase_admin._apps:
+            cred = credentials.Certificate("./../data/database_cred.json")
+            firebase_admin.initialize_app(cred)
+            self.db = firestore.client()
 
     def add_new_webpage(self, ref, data):
         doc_ref = self.db.collection('pages').document(ref)
@@ -17,31 +18,15 @@ class DatabaseManager:
             return doc_ref.set(data)
 
     def get_webpage(self, ref):
-        return self.db.collection('pages').document(ref).get()
+        if self.db.collection('pages').document(ref).get().exists:
+            return self.db.collection('pages').document(ref).get()
+        else:
+            return None
 
     def update_web_page(self, ref, data):
         if self.db.collection('pages').document(ref).get().exists:
             self.db.collection('pages').document(ref).update(data)
 
     def delete_web_page(self, ref):
-        if  self.db.collection('pages').document(ref).get().exists:
+        if self.db.collection('pages').document(ref).get().exists:
             self.db.collection('pages').document(ref).delete()
-
-if __name__ == '__main__':
-    db = DatabaseManager()
-    ref = db.add_new_webpage('firstpage', {
-        'home_url': 'https://www.firstpage.com',
-        'login_url': 'https://www.firestpage.com/login',
-        'register_url': 'https://www.firstpage.com/register',
-        'register_data': {
-            'user_name': 'cookiehunter',
-            'email': 'cookiehunterproject+firstpage@gmail.com',
-            'password': 'passwordRandom123!',
-            'name': 'Janssen'
-        },
-        'registered': False,
-        'able_to_fill_register': False,
-        'able_to_fill_login': False,
-        'activated': False,
-        'captcha': False
-    })
