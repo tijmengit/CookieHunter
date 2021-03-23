d2d0fdd62e63bb2dc80b265591ceb62f8e9f5abe
tijmen104 <tijmen104@gmail.com>
2021-03-23 09:45:26 +0100

    Relocate for loop in main

--- src/__main__.py
+++ src/__main__.py
@@ -4,60 +4,55 @@ from CookieAuditor import *
 from PrivacyAuditor import *
 import json
 
-def fullFlow(pages):
-    PATH = os.getenv(
-        "LOCALAPPDATA") + "/ChromeDriver/chromedriver" if platform.system() == "Windows" else "/usr/local/sbin/chromedriver"
+def fullFlow(page, PATH, privacy_auditor):
 
-    privacy_auditor = PrivacyAuditor(PATH, ['--headless'])
+    try:
+        browser = Browser(page['home_url'], page['login_url'], page['register_url'], PATH)
+        ref = browser.identifier
+        document = browser.db.get_webpage(ref)
+        if not document:
+            raise Exception(f'Could not fetch document for: {ref}')
 
-    for page in pages:
+        # REGISTER
+        register_in_db = False
         try:
-            browser = Browser(page['home_url'], page['login_url'], page['register_url'], PATH)
-            ref = browser.identifier
+            register_in_db = document['registered']
+        except Exception as e:
+            pass
+        if not register_in_db:
+            registered = browser.register()
+            if not registered:
+                raise Exception(f'Could not register on page: {page}')
             document = browser.db.get_webpage(ref)
             if not document:
                 raise Exception(f'Could not fetch document for: {ref}')
 
-            # REGISTER
-            register_in_db = False
-            try:
-                register_in_db = document['registered']
-            except Exception as e:
-                pass
-            if not register_in_db:
-                registered = browser.register()
-                if not registered:
-                    raise Exception(f'Could not register on page: {page}')
-                document = browser.db.get_webpage(ref)
-                if not document:
-                    raise Exception(f'Could not fetch document for: {ref}')
+        # LOGIN
+        creds = document['register_data']
+        browser.login()
+        if not browser.login_oracle():
+            raise Exception(f'Could not login for: {page}')
 
-            # LOGIN
-            creds = document['register_data']
-            browser.login()
-            if not browser.login_oracle():
-                raise Exception(f'Could not login for: {page}')
+        # COOKIE AUDIT
+        cookie_auditor = CookieAuditor(browser)
+        vulnerable = cookie_auditor.findVulnerableCookies()
+        browser.db.update_web_page(ref, {"vulnerable": vulnerable})
+        vulnerable_cookies = []
+        if vulnerable['secure'] or vulnerable['httpOnly']:
+            vulnerable_cookies = cookie_auditor.findAuthCookies()
 
-            # COOKIE AUDIT
-            cookie_auditor = CookieAuditor(browser)
-            vulnerable = cookie_auditor.findVulnerableCookies()
-            browser.db.update_web_page(ref, {"vulnerable": vulnerable})
-            vulnerable_cookies = []
-            if vulnerable['secure'] or vulnerable['httpOnly']:
-                vulnerable_cookies = cookie_auditor.findAuthCookies()
+        if not vulnerable_cookies:
+            browser.db.update_web_page(ref, {"leaks": []})
+            return
+        browser.close()
 
-            if not vulnerable_cookies:
-                browser.db.update_web_page(ref, {"leaks": []})
-                continue
-            browser.close()
+        # PRIVACY AUDIT
+        leaks = privacy_auditor.audit(page['home_url'], vulnerable_cookies[0], creds)
+        browser.db.update_web_page(ref, {"leaks": leaks})
 
-            # PRIVACY AUDIT
-            leaks = privacy_auditor.audit(page['home_url'], vulnerable_cookies[0], creds)
-            browser.db.update_web_page(ref, {"leaks": leaks})
-
-        except:
-            traceback.print_exc()
-            browser.close()
+    except:
+        traceback.print_exc()
+        browser.close()
 
 
 if __name__ == "__main__":
@@ -71,5 +66,8 @@ if __name__ == "__main__":
     # ]
     with open('../data/sites.json') as file:
         pages = json.load(file)
-
-    fullFlow(pages)
+    PATH = os.getenv(
+        "LOCALAPPDATA") + "/ChromeDriver/chromedriver" if platform.system() == "Windows" else "/usr/local/sbin/chromedriver"
+    privacy_auditor = PrivacyAuditor(PATH, ['--headless'])
+    for page in pages:
+        fullFlow(page, PATH, privacy_auditor)
