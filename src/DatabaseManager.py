7d7e1320571932950ad983acaa4f978542230d13
schorliee <da.charlie.nguyen@gmail.com>
2021-03-23 22:45:34 +0100

    make collections for DBmanager more generic

--- src/DatabaseManager.py
+++ src/DatabaseManager.py
@@ -1,7 +1,8 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-
+#collection_name = 'pages'
+collection_name = 'alexa_test'
 class DatabaseManager:
 
     def __init__(self):
@@ -11,22 +12,22 @@ class DatabaseManager:
         self.db = firestore.client()
 
     def add_new_webpage(self, ref, data):
-        doc_ref = self.db.collection('pages').document(ref)
+        doc_ref = self.db.collection(collection_name).document(ref)
         if doc_ref.get().exists:
             return doc_ref
         else:
             return doc_ref.set(data)
 
     def get_webpage(self, ref):
-        if self.db.collection('pages').document(ref).get().exists:
-            return self.db.collection('pages').document(ref).get().to_dict()
+        if self.db.collection(collection_name).document(ref).get().exists:
+            return self.db.collection(collection_name).document(ref).get().to_dict()
         else:
             return None
 
     def update_web_page(self, ref, data):
-        if self.db.collection('pages').document(ref).get().exists:
-            self.db.collection('pages').document(ref).update(data)
+        if self.db.collection(collection_name).document(ref).get().exists:
+            self.db.collection(collection_name).document(ref).update(data)
 
     def delete_web_page(self, ref):
-        if self.db.collection('pages').document(ref).get().exists:
-            self.db.collection('pages').document(ref).delete()
+        if self.db.collection(collection_name).document(ref).get().exists:
+            self.db.collection(collection_name).document(ref).delete()
