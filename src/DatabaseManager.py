dad39bcd9c7d06dae413d7c5ac6016ffefb457ef
Stijn Kramer <stijn_kramer@hotmail.com>
2021-03-26 19:04:41 +0100

    cleanup on aisle 6; added typing and general cleanup; small performance improvement in browser.__generic_element_finder

--- src/DatabaseManager.py
+++ src/DatabaseManager.py
@@ -1,9 +1,12 @@
+from typing import Dict, Any, Optional
+
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-#collection_name = 'pages'
-collection_name = 'alexa_test'
+
+
 class DatabaseManager:
+    collection_name = 'alexa_test'
 
     def __init__(self):
         if not firebase_admin._apps:
@@ -11,23 +14,23 @@ class DatabaseManager:
             firebase_admin.initialize_app(cred)
         self.db = firestore.client()
 
-    def add_new_webpage(self, ref, data):
-        doc_ref = self.db.collection(collection_name).document(ref)
+    def add_new_webpage(self, ref: str, data: Dict[str, Any]) -> Any:
+        doc_ref = self.db.collection(self.collection_name).document(ref)
         if doc_ref.get().exists:
             return doc_ref
         else:
             return doc_ref.set(data)
 
-    def get_webpage(self, ref):
-        if self.db.collection(collection_name).document(ref).get().exists:
-            return self.db.collection(collection_name).document(ref).get().to_dict()
+    def get_webpage(self, ref: str) -> Optional[Dict[str, Any]]:
+        if self.db.collection(self.collection_name).document(ref).get().exists:
+            return self.db.collection(self.collection_name).document(ref).get().to_dict()
         else:
             return None
 
-    def update_web_page(self, ref, data):
-        if self.db.collection(collection_name).document(ref).get().exists:
-            self.db.collection(collection_name).document(ref).update(data)
+    def update_web_page(self, ref: str, data: Dict[str, any]) -> None:
+        if self.db.collection(self.collection_name).document(ref).get().exists:
+            self.db.collection(self.collection_name).document(ref).update(data)
 
-    def delete_web_page(self, ref):
-        if self.db.collection(collection_name).document(ref).get().exists:
-            self.db.collection(collection_name).document(ref).delete()
+    def delete_web_page(self, ref: str) -> None:
+        if self.db.collection(self.collection_name).document(ref).get().exists:
+            self.db.collection(self.collection_name).document(ref).delete()
